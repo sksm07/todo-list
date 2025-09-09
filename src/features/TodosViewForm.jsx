f8309be72dca6902d29d2de9bcdb023701ca8083
swetha.k <swetha_kanneganti@yahoo.co.in>
2025-09-09 11:26:20 -0400

    paused API requests while the user is typing

--- src/features/TodosViewForm.jsx
+++ src/features/TodosViewForm.jsx
@@ -1,18 +1,31 @@
+import { useState, useEffect } from "react";
+
 function TodosViewForm ({sortDirection, setSortDirection, 
                         sortField, setSortField, queryString, setQueryString}) {
     function preventRefresh(event){
         event.preventDefault()
     }
+    
+    const [localQueryString, setLocalQueryString] = useState(queryString);
+    
+    useEffect(() => {
+        const debounce = setTimeout(()=>{
+            setQueryString(localQueryString)
+        }, 500)
+        
+        return ()=>{clearTimeout(debounce)}
+    },[localQueryString, setQueryString])
+
     return (
         <form onSubmit={preventRefresh}>
             <div>
                 <label>
                     Search todos
-                    <input type="text" value={queryString} 
-                        onChange={(e)=>{setQueryString(e.target.value)}}
+                    <input type="text" value={localQueryString} 
+                        onChange={(e)=>{setLocalQueryString(e.target.value)}}
                     ></input>
                 </label>
-                <button type="button" onClick={()=>setQueryString("")}>Clear</button>
+                <button type="button" onClick={()=>setLocalQueryString("")}>Clear</button>
             </div>
             <div>
                 <label>
